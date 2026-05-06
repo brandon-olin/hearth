b4d4d1743ea0320e32f724e5e4219067b0cd28c3
Brandon Olin <olin.brandon@gmail.com>
2026-05-06 11:30:06 -0400

    fix: update down_revision reference in migration 0008 to match previous revision

--- api/migrations/versions/0008_add_recipe_body.py
+++ api/migrations/versions/0008_add_recipe_body.py
@@ -1,7 +1,7 @@
 """add recipe body jsonb
 
 Revision ID: 0008
-Revises: 0007
+Revises: f1c8b3e6a9d2
 Create Date: 2026-05-06
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import JSONB
 
 revision = "0008"
-down_revision = "0007"
+down_revision = "f1c8b3e6a9d2"
 branch_labels = None
 depends_on = None
 
